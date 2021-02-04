c29853fb65b0f670404bd2fa6a90ef0886d4c0b2
Le Chi <le.khanhchi@yahoo.fr>
2021-02-04 21:54:15 +0100

    update localStorage

--- paiementAvecJs.js
+++ paiementAvecJs.js
@@ -1,56 +1,141 @@
 
         // charger la page paiement.html avec les éléments du localStorage;
+        
         function displayCart() {
             let cartItems = localStorage.getItem('productInCart'); // on récupère les produits dans localstorage
             cartItems = JSON.parse(cartItems);
             let productContainer = document.getElementById('chargementPanier');
+            let productTotal = document.getElementById('tfoot');
+
             let cartCost = localStorage.getItem('totalCost'); // la somme total des articles
             cartCost = parseInt(cartCost);
 
             if( cartItems && productContainer ) { // s'il y a le contenu dans le storage et aussi on est sur la page paiement
-                productContainer.innerHTML = '';
-                Object.values(cartItems).map (item => { // on ajoute dans le HTML ce contenu
-                    productContainer.innerHTML += ` 
-                    <tr>
-                        <td class="col-md-2 produit_paiement"><img class="img-fluid img-thumbnail " src="${item.photo}" alt="${item.name}"/></td>
-                        <td class="col-2">${item.name}</td>
-                        <td class="col-1">
-                            <i class="fas fa-plus-circle addition"></i> <span class="quantity"> ${item.inCart} </span><i class="fas fa-minus-circle soustraction"></i>
-                            
-                        </td>
-                        <td class="col-1"><p class="fw-bold">${item.price}</p></td>
-                        <td class="col-1"><p class="fw-bold">${item.price * item.inCart}</p></td>
-                    </tr>
-                    `
-                });
+               
+                Object.values(cartItems).map(item => {
+                    let tr = document.createElement('tr');
 
-                //on ajoute la somme total dans le localStorage
-                productContainer.innerHTML += `
-                    <table class="table m-auto table-responsive-sm">
-                        <thead>
-                            <tr>   
-                                <th class="col-md-2 produit_paiement"></th>
-                                <th class="col-1"></th>
-                                <th class="col-1"></th> 
-                                <th class="col-2">Sous total</th>
-                                <th class="col-1">${cartCost}</th>
-                            </tr>
-                        </thead>
-                    </table>
-                `
+                    let td1 = document.createElement('td');
+                        td1.setAttribute('class','col-md-2 produit_paiement')
+                
+                    let img = document.createElement('img');
+                        img.setAttribute('class', 'img-fluid img-thumbnail')
+                        img.setAttribute('src',`${ item.photo}`)
+                    td1.appendChild(img)
+
+                    let td2 = document.createElement('td');
+                        td2.setAttribute('class','col-2')
+                        td2.innerHTML= item.name;
+
+                    let td3 = document.createElement('td');
+                         td3.setAttribute('class','col-1')
+                    
+                    let i1 = document.createElement('i');
+                        i1.addEventListener('click',() => addition(item));
+                        i1.setAttribute('class','fas fa-plus-circle addition');
+                    td3.append(i1);
+
+                    let span = document.createElement('span');
+                        span.setAttribute('class','quantity');
+                        span.setAttribute('id',item.id);
+                        span.innerHTML = item.inCart ;
+                    td3.append(span);
+
+                    let i2 = document.createElement('i');
+                        i2.addEventListener('click',() => soustraction(item));
+                        i2.setAttribute('class','fas fa-minus-circle soustraction');
+                    td3.append(i2);
+
+                    let td4 = document.createElement('td');
+                        td4.setAttribute('class','col-1');
+                        td4.innerHTML= item.price;
+
+                    let td5 = document.createElement('td');
+                        td5.setAttribute('class','col-1')
+                        td5.setAttribute('class','somme')
+
+                        td5.innerHTML= item.price * item.inCart ;
+
+                    tr.append(td1)
+                    tr.append(td2)
+                    tr.append(td3)
+                    tr.append(td4)
+                    tr.append(td5)
+                    productContainer.append(tr);
+
+                    // on ajoute la somme total dans le localStorage
+
+                    productTotal.innerHTML = cartCost;
+
+                })
             }
 
-            // on modifie la quantité des articles et modifier la somme total
-            let addition = document.querySelector('.addition');
-            let soustraction = document.querySelector('.soustraction');
-            let quantity = document.querySelector('.quantity');
-                    addition.addEventListener ('click', () => {
-                        console.log(cartItems[product.name].inCart)
-                    })
-        }
+            
+}   
 
+displayCart();
+
+
+let cart = localStorage.getItem('productInCart');//saisir les produits dans localStorage
+cart = JSON.parse(cart);
+let tab = Object.values(cart); // convertir ses valeurs en tableau des objects
+
+function addition (item) { // ajouter 1 quand on click sur + et mise à jour localStorage
         
+    let cart = localStorage.getItem('productInCart');
+    cart= JSON.parse(cart);
+    cart[item.name]['inCart'] +=1;
+    localStorage.setItem('productInCart', JSON.stringify(cart));
+
+    
+    updatetotalCost()
+    updatecartNumber ()
+   
+}
+
+function soustraction (item) {//enlever 1 quand on click sur - et mise à jour localStorage
+    let cart = localStorage.getItem('productInCart');
+    cart= JSON.parse(cart);
+    cart[item.name]['inCart'] -=1;
+    localStorage.setItem('productInCart', JSON.stringify(cart));
+
+    
+    updatetotalCost()
+    updatecartNumber ()
+
+}
+
+function updatetotalCost() {// mise à jour la valeur total pour ensemble des produits
+    let s=0; 
+    for(let i=0; i<tab.length; i++) {
+        tab[i].total= tab[i].price * tab[i].inCart;
         
+        s += tab[i].total;
+    }
+    localStorage.setItem('totalCost', JSON.stringify(s));
+    let sousTotal = document.getElementById('tfoot');
+    let somme = localStorage.getItem('totalCost');
+    somme= JSON.parse(somme);
+    sousTotal.innerHTML = somme;
+}
 
-    displayCart()
+function updatecartNumber () {
+    let x=0; 
+    for(let i=0; i<tab.length; i++) {
+        x += tab[i].inCart
+    }
+    localStorage.setItem('cartNumber', JSON.stringify(x));
+    let sum = JSON.parse(localStorage.getItem('cartNumber'));
+    
+    let panier = document.getElementById('in-cart-items-num');
+    panier.innerHTML= sum;
+}
 
+updatetotalCost()
+updatecartNumber ()
+
+
+
+
+    
+    
