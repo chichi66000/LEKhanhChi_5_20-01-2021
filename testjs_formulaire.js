5f0e90f79672d46787d5a937830c5d2ac059727c
Le Chi <le.khanhchi@yahoo.fr>
2021-02-06 23:11:41 +0100

    pas encore réussir à post avec fetch

--- testjs_formulaire.js
+++ testjs_formulaire.js
@@ -1,6 +1,16 @@
 
-    let forms = document.getElementById('formulaire');
-    forms.addEventListener ('submit', function valider (e) {
+
+function updatePanier () {
+    // let totalCost = parseInt(localStorage.getItem('totalCost'));
+    let cartNumber = parseInt(localStorage.getItem('cartNumber'));
+    // let totalProduit= document.querySelector('.totalProduit'); 
+    // totalProduit.innerHTML = totalCost;// afficher le total des produits quand la page se rafraichir;  
+    document.getElementById('in-cart-items-num').innerHTML = cartNumber;
+}
+updatePanier ()
+   
+   let formumaire = document.getElementById('formulaire');
+    formulaire.addEventListener ('submit', function valider (e) {
         e.preventDefault();
 
         let nom = document.getElementById('nom');
@@ -21,7 +31,7 @@
             let text_validation = /^[a-zA-ZéèîïÉÈÊËÍÌÎÏ][a-zéèîïàçâäêëôöùûüÿ]+([-'\s][a-zA-ZéèîïÉÈÊËÍÌÎÏ][a-zéèîïàçâäêëôöùûüÿ]+)?$/;
             let tel_validation = /[0-9]{10}/;
             let adresse_validation = /^[0-9]+[\s]+[a-zA-ZéèîïÉÈÊËÍÌÎÏ][a-zéèîïàçâäêëôöùûüÿ]/;
-            let ville_validation = /^[a-zA-ZéèîïÉÈÊËÍÌÎÏ][a-zéèîïàçâäêëôöùûüÿ]+/;
+            let ville_validation = /^[a-zA-ZéèîïÉÈÊËÍÌÎÏ][a-zaàiéèîïàçâäêëôöùûüÿ]/;
             let pays_validation = /France|Belgique|Allemagne|Espagne]/i;
             let codePostal_validation = /[0-9]{5}/;
             let email_validation = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
@@ -48,9 +58,36 @@
 
             else if ( pays_validation.test (pays.value) == false) { e.preventDefault (); e.stopPropagation (); alert("pays: incorrect");}
         
-            else { alert("formulaire envoyé avec succès");}
+            else { 
+                let headers = new Headers();
+                headers.set('Accept', 'application/json');
+
+                let formData = new FormData();
+                for ( let i=0; i< formulaire.length; ++i) {
+                    formData.append(formulaire[i].name, formulaire[i].value)// ajouter les valeurs du formulaire dans formData;
+                }
+
+                let formDataJson = JSON.stringify(formData);
+
+                let articleAchete = JSON.stringify(localStorage.getItem('productInCart'));
+                formData.append('json', articleAchete) // ajouter les produits acheté dans formData;
+
+
+                let optionFetch = {
+                    method: 'POST',
+                    headers,
+                    body: formDataJson,
+                }
+                let response = fetch ('http://localhost:3000/api/teddies', optionFetch); 
+
+                if(! response.ok) {
+                    let messageErreur = response.text;
+                    throw new Error(messageErreur);
+                }
+                e.href = "confirmationJs.html";
+            }
         })
 
-            
+   
 
 
