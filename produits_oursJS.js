bd92da0dc0048ba96c0a7594a30222379c22790d
Le Chi <le.khanhchi@yahoo.fr>
2021-02-07 23:02:55 +0100

    ok

--- produits_oursJS.js
+++ produits_oursJS.js
@@ -1,3 +1,4 @@
+// récupérer les information du serveur
 let promise = fetch('http://localhost:3000/api/teddies')
     .then (response => response.json())
         .then (data => chargement (data))
@@ -7,34 +8,39 @@ let promise = fetch('http://localhost:3000/api/teddies')
         let ours = data;
         
         for(let i=0;i<ours.length; i++) {
-            
+            // on créer des éléments HTML avec JS
             let containeur = document.querySelector('.containeur');
+
             let teddy = document.createElement ('div');
                 teddy.setAttribute('class','col-sm-10 col-md-5 flex-column m-auto mb-3 text-center');
                 containeur.appendChild(teddy);
+
             let link = document.createElement ('a');
                 link.setAttribute('class','card produit bg-light ours');
                 teddy.appendChild(link);
                 link.href='teddyEnJs.html' + '?id=' + ours[i]._id;
+
             let photoTeddy = document.createElement ('img');
                 photoTeddy.setAttribute('class','card-img-top h-75');
                 photoTeddy.setAttribute('alt', ours[i].name)
                 link.appendChild(photoTeddy);
                 photoTeddy.src=ours[i].imageUrl;
-            let ajoutPanier = document.createElement('h5');
-                ajoutPanier.setAttribute('class','card-text hide p-1');
-                ajoutPanier.innerHTML = 'Ajouter au panier';
-                link.appendChild(ajoutPanier);
+            
             let titre = document.createElement('h4');
                 titre.setAttribute('class','card-title');
                 titre.innerHTML = ours[i].name;
                 link.appendChild(titre);
+
             let para = document.createElement('p');
                 para.setAttribute('class','card-text ls-2 fw-bold');
                 para.innerHTML = "Prix : " + ours[i].price;
                 link.appendChild(para);
         }
-       
 
     }   
-    // importer la fonction onLoadCartNumber() depuis fichier teddyEnJs.js???
+    // afficher le nombre d'article dans le panier s'il y en a
+    let inCart = document.getElementById('in-cart-items-num');
+    if( localStorage.getItem('cartNumber')) {
+        inCart.innerHTML = localStorage.getItem('cartNumber')
+    }
+    else {inCart.innerHTML = ""}
